b9568fef298270dc093a2a768875939e72d2362d
Julian Wong <julian.wong@lloydsbanking.com>
2024-07-10 15:44:58 +0100

    Removed gcloud auth login from port-forwarding script

--- google-cloud-sql-connect.sh
+++ google-cloud-sql-connect.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Get Google Cloud credentials and set the correct K8s context.
-gcloud auth login
 gcloud container clusters get-credentials tnt01-audmsa-bld-01-kcl-01-euwe2 --region=europe-west2 --project=tnt01-audmsa-bld-01-fdc2
 gcloud config set project tnt01-audmsa-bld-01-fdc2
 
